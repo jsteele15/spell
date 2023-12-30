bd467037c959d6ffc20a34c0270c9b79fd0df242
Jimbo Jones <128925976+jsteele15@users.noreply.github.com>
2023-12-30 15:48:45 +0000

    animation
    added animation to the code, as well as new button functionality

--- New folder/fight.js
+++ New folder/fight.js
@@ -25,6 +25,9 @@ const mS = document.getElementById('monS');
 //this is to check the orientation of the screen
 let ori;
 
+//check the currect level
+let curLevel = 0;
+
 window.addEventListener("resize", function(){
     if(window.matchMedia('(orientation: portrait)').matches === true){
         ori = "portrait";
@@ -55,28 +58,95 @@ window.addEventListener('load', function(){
 let monImage = new Image();
 monImage.src = "images/mon.png";
 
+let mon2 = new Image();
+mon2.src = "images/mon-export2.png";
+
 let attImage = new Image();
 attImage.src = "images/fire.png";
+
+let buttImage = new Image();
+buttImage.src = "images/fire.png";
+
+//function for the ui
+//need to check if clicked in
+function Butts(ind, imageCut, pos, size){
+    this.ind = ind;
+    this.imageCut = imageCut;
+    this.pos = pos;
+    this.size = size;
+
+    this.draw = function(){
+        if (ori === "landscape"){
+            //ctx.clearRect(0, 0, canvas.width, canvas.height);
+            ctx.drawImage(buttImage, imageCut[0], imageCut[1], 100, 100,pos[0], pos[1], 100, 100);
+        } else {
+            //ctx.clearRect(0, 0, canvas.width, canvas.height);
+            ctx.drawImage(buttImage, imageCut[0], imageCut[1], 100, 100,pos[0], pos[1], size[0], size[1]);
+        }
+    }
+}
+const start = new Butts(1, [0, 0], [40, 40], [400, 400])
+const lv2 = new Butts(2, [0, 0], [300, 600], [400, 400])
+
+let buttList = [start, lv2];
+
+document.addEventListener('click', function(event) {
+    var x = event.clientX;
+    var y = event.clientY;
+    console.log('x: ' + x + ', y: ' + y);
+    for(let i = 0; i < buttList.length; i++){
+        if (x >= buttList[i].pos[0] && x <= buttList[i].pos[0]+ buttList[i].size[0]){
+            if (y >= buttList[i].pos[1] && y <= buttList[i].pos[1]+ buttList[i].size[1]){
+                console.log(buttList[i].ind)
+        };
+        };
+    };
+  });
+  
 
 //i want to monsters to cycle in and out after getting deffeated
 //i need some random chose of monster
 //maybe it ends after killing ten monsters
 //maybe the monsters get harder over time
 
-function Monster(name, attack, imageCut, health, score){
+function Monster(image, name, attack, imageCut, health, score, frame, maxFrames){
+    this.image = image;
     this.name = name;
     this.attack = attack;
     this.imageCut = imageCut;
     this.health = health;
     this.score = score;
+    this.frames = frame;
+    this.initialFrame = frame;
+    this.maxFrames = maxFrames;
     
     this.draw = function(){
         if (ori === "landscape"){
             ctx.clearRect(0,0, canvas.width, canvas.height);
-            ctx.drawImage(monImage, imageCut[0], imageCut[1], 460, 500,scrW/2-230, -40, 460, 500);
+            ctx.drawImage(image, imageCut[0], imageCut[1]*frames, 460, 500,scrW/2-230, -40, 460, 500);
         } else {
+            //every time we turn the rotation it speeds up likely due to the draw function
+            //being called multiple times without clearing the interval
+            let inter = setInterval(() => {
             ctx.clearRect(0,0, canvas.width, canvas.height);
-            ctx.drawImage(monImage, imageCut[0], imageCut[1], 460, 500,scrW/2-460, -40, 920, 1000);
+            ctx.drawImage(image, imageCut[0], imageCut[1]*frames, 460, 500,scrW/2-460, -40, 920, 1000);
+            
+            //counter();
+            //if(frames >= 100){
+                //clearInterval(counter);
+                //console.log("cleared")
+            //}
+            if(this.health <= 0){
+                console.log("interval cleared");
+                clearInterval(inter);
+            }
+        
+            if(frames < maxFrames){
+                ++frames;
+            } else {
+                frames = this.initialFrame;
+            }
+            }, 200);
         }
     }
     this.att = function(){
@@ -107,13 +177,16 @@ function Monster(name, attack, imageCut, health, score){
         //else if (char.dodge === true){
             //console.log("dodged") 
 };
-let wormyBoy = new Monster('wormy boy', 10, [460, 0], 10, 10000);
-let wormyBoy1 = new Monster('wormy boy1', 10, [460, 0], 10, 10000);
-let wormyBoy2 = new Monster('wormy boy2', 10, [460, 0], 10, 10000);
-let wormyBoy3 = new Monster('wormy boy3', 10, [460, 0], 10, 10000);
-let badDrawnFella = new Monster('sad man', 10, [0, 0], 10, 20000);
+let wormyBoy = new Monster(mon2, 'wormy boy', 10, [460, 500], 10, 10000, 14, 19);
+let wormyBoy1 = new Monster(monImage, 'wormy boy1', 10, [460, 0], 10, 10000,1, 1);
+let wormyBoy2 = new Monster(monImage, 'wormy boy2', 10, [460, 0], 10, 10000, 1, 1);
+let wormyBoy3 = new Monster(monImage, 'wormy boy3', 10, [460, 0], 10, 10000, 1, 1);
+let badDrawnFella = new Monster(monImage, 'sad man', 10, [0, 0], 10, 20000,1 , 1);
 
 let monList = [wormyBoy, badDrawnFella, wormyBoy1, wormyBoy2, wormyBoy3];
+//the list of lists will need implementing at some point
+let listLevs = [buttList, monList];
+
 let curInd = 0;
 
 /*
@@ -127,11 +200,17 @@ let save = 0;
 let level = 0
 
 let battle = function(enList){
-    //need to fill in this i guess
-    //
     sUI.innerHTML = scr;
     
-    if(enList[curInd].health >= 1){
+    //this check if the player has gone through the list of enemies
+    if(curInd >= enList.length){
+        console.log("you killed them all");
+        for(let i = 0; i < buttList.length; i++){
+            buttList[i].draw();
+        };
+    }
+
+    else if(enList[curInd].health >= 1){
         enList[curInd].draw();
         //if the monster is alive, and the battle is on, this will cause them to attack every interval
         console.log('set interval');
@@ -152,6 +231,7 @@ let battle = function(enList){
             battle(enList);
         } else {
             console.log("you killed them all");
+            buttList.draw();
         };
     }
 };
@@ -181,6 +261,12 @@ window.onload= function(){
 */
 //this is a function that starts the game if the button is clicked
 const starBut = document.getElementById("menuStart");
+
+window.onload = function(){
+    start.draw();
+    lv2.draw();
+    console.log("jndiub");
+};
 
 starBut.addEventListener("click", function(){
     gameScene = true;
